45da4bbfe220f47d1c37921cb7c5d644800db3a4
hiroka <mitsunagahrk87@gmail.com>
2024-10-13 11:31:44 +0900

    section7 34

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, TypeScript!");
+import express from "express";
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/api/hello", async (req, res) => {
+  res.json({
+    message: "Hello Express",
+  });
+});
+
+//3000番ポートでサーバーの起動を試みて、成功したらログ出力してる。
+app.listen(PORT, () => {
+  console.log(`Reversi application started: http://localhost:${PORT}`);
+});
