90721083aa35a3fc4cc02538427f3582b4b811b1
hiroka <mitsunagahrk87@gmail.com>
2024-10-20 15:15:02 +0900

    section11 62

--- src/presentation/gameRouter.ts
+++ src/presentation/gameRouter.ts
@@ -1,37 +1,12 @@
 import express from "express";
-import { GameGateway } from "../dataaccess/gameGateway.js";
-import { TurnGateway } from "../dataaccess/turnGateway.js";
-import { SquareGateway } from "../dataaccess/squareGateway.js";
-import { connectMySQL } from "../dataaccess/connection.js";
-import { DARK, INITIAL_BOARD } from "../application/constants.js";
+import { GameServive } from "../application/gameService.js";
 
 export const gameRouter = express.Router();
 
-const gameGateway = new GameGateway();
-const turnGateway = new TurnGateway();
-const squareGateway = new SquareGateway();
+const gameService = new GameServive();
 
 gameRouter.post("/api/games", async (req, res) => {
-  const now = new Date();
-
-  const conn = await connectMySQL();
-  try {
-    await conn.beginTransaction();
-
-    const gameRecord = await gameGateway.insert(conn, now);
-    const turnRecord = await turnGateway.insert(
-      conn,
-      gameRecord.id,
-      0,
-      DARK,
-      now
-    );
-    await squareGateway.insertAll(conn, turnRecord.id, INITIAL_BOARD);
-
-    await conn.commit();
-  } finally {
-    await conn.end();
-  }
+  await gameService.startGame();
 
   res.status(201).end();
 });
